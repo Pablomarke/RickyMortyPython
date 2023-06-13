152a5b200919bd7dbca0c2e494361565ea1a9630
Pablo Márquez Marín <pablomarquezmarin@MacBook-Pro-de-Pablo.local>
2023-06-13 15:42:10 +0200

    Creadas las paginas para localizaciones y episodios. creados en routes para poder previsualizar

--- rym/routes.py
+++ rym/routes.py
@@ -1,25 +1,32 @@
 from rym import app
 from flask import render_template
 from rym.models import RyMAPI
+import random
 
 rm = RyMAPI()
 
 @app.route("/")
 def index():
+    random_number = random.randint(1,800)
     
     return render_template("index.html", 
-                           title="Inicio")
+                           number=random_number)
 
 @app.route("/characters")
 def character():
     personaje = rm.allCharacters()
     personajes = personaje["results"]
-    """nombre = personaje["name"]
-    estado = personaje["status"]
-    especie = personaje["species"]
-    genero = personaje["gender"]
-    datos = [nombre, estado, especie,genero]"""
 
     return render_template("characters.html",
                            title="Listado de personajes",
                            data= personajes)
+
+@app.route("/locations")
+def location():
+    return render_template("locations.html",
+                           title = "Localizaciones")
+
+@app.route("/episodes")
+def episode():
+    return render_template("episodes.html",
+                           title = "Episodios")
